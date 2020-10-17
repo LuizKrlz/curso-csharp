e0263e232be3d7ce78209cdc05b3ca90c3ef41ba
Luiz Carlos <luiz.correa.dev@gmail.com>
2020-10-17 14:27:20 -0300

    start

---
+++ 01Conceitos/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Pessoa
+{
+    //Atributos
+    public string nome;
+
+    public int idade;
+
+    //Metodos
+    public void mensagem()
+    {
+        Console.WriteLine("Olá " + nome + "voce tem " + idade + " anos");
+
+    }
+}
